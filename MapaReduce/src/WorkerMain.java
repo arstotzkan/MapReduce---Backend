fbe1db9f694ae929beb8694d47bc29fcb85a67a7
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-04-14 17:50:13 +0300

    now we create many workers

--- MapaReduce/src/WorkerMain.java
+++ MapaReduce/src/WorkerMain.java
@@ -3,6 +3,14 @@ import UserClient.WorkerClient;
 public class WorkerMain {
 
     public static void main(String[] args) {
-        new WorkerClient().start();
+        //you need to specify number of workers via cmd
+        int numberOfWorkers = Integer.parseInt(args[0])
+        WorkerClient[] workers = WorkerClient[numberOfWorkers];
+
+        for (int i = 0; i < workers.length; i++){
+            workers[i] = new WorkerClient() //worker init
+            workers[i].run();
+        }
+
     }
 }
