3b59a1419a01ccd696bd4e1b6f436098f18ea846
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-04-19 20:18:01 +0300

    fixed paths

--- MapaReduce/src/UserMain.java
+++ MapaReduce/src/UserMain.java
@@ -2,8 +2,8 @@ import UserClient.UserClient;
 
 public class UserMain {
     public static void main(String[] args) {
-        new UserClient("gpxs\\route1.gpx").start();
-        new UserClient("gpxs\\route2.gpx").start();
-        new UserClient("gpxs\\route3.gpx").start();
+        new UserClient("src\\gpxs\\route1.gpx").start();
+        new UserClient("src\\gpxs\\route2.gpx").start();
+        new UserClient("src\\gpxs\\route3.gpx").start();
     }
 }
