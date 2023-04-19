fa4221223a18f2ee8102170b9ced3cb6cbe98878
Kharnifex <61503559+Kharnifex@users.noreply.github.com>
2023-04-19 18:58:33 +0300

    parser now takes string of raw gpx data as input instead of a string that contains the path of the .gpx file

--- MapaReduce/src/utils/GPXParser.java
+++ MapaReduce/src/utils/GPXParser.java
@@ -1,7 +1,9 @@
 package utils;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,37 +23,35 @@ import org.xml.sax.*;
  */
 public class GPXParser {
 
-    private static Document readGPX(String gpxPath) throws ParserConfigurationException, SAXException, IOException {
-        File gpx = new File(gpxPath);
+    private static Document readGPX(String gpx) throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(gpx);
-        doc.getDocumentElement().normalize();
-        return doc;
+        InputSource is = new InputSource(new StringReader(gpx));
+        return builder.parse(is);
     }
 
     /**
      * Parses a .gpx file and returns a list of GPXWaypoints
      *
-     * @param gpxPath the Path of the gpx file
+     * @param gpx a string containing raw data of a .gpx file
      * @return ArrayList of GPXWaypoints
      * @throws ParserConfigurationException
      * @throws IOException
      * @throws SAXException
      * @throws ParseException
      */
-    public static ArrayList<GPXWaypoint> parseGPX(String gpxPath) throws ParserConfigurationException, IOException, SAXException, ParseException {
-        Document doc = readGPX(gpxPath);
+    public static ArrayList<GPXWaypoint> parseGPX(String gpx) throws ParserConfigurationException, IOException, SAXException, ParseException {
+        Document doc = readGPX(gpx);
         NodeList nodeList = doc.getElementsByTagName("*");
-        ArrayList<GPXWaypoint> waypoints = new ArrayList<GPXWaypoint>();
+        ArrayList<GPXWaypoint> waypoints = new ArrayList<>();
         String user = null; //not sure where we'll be using this but once we have DAOs its useful
 
         for (int i = 0; i < nodeList.getLength(); i++) {
 
-            String tempLat = null;
-            String tempLon = null;
-            String tempEle = null;
-            String tempTime = null;
+            String tempLat;
+            String tempLon;
+            String tempEle;
+            String tempTime;
 
             Node currNode = nodeList.item(i);
 
@@ -60,15 +60,13 @@ public class GPXParser {
 
                 if (currNode.getNodeName().equals("gpx")) {
                     user = element.getAttribute("creator");
-                }
-
-                else if (currNode.getNodeName().equals("wpt")){
+                } else if (currNode.getNodeName().equals("wpt")) {
                     tempLat = element.getAttribute("lat");
                     tempLon = element.getAttribute("lon");
 
                     String textContent = currNode.getTextContent().replace("\n", "");
                     String[] textArr = textContent.split(" ");
-                    List<String> filtered =  Arrays.stream(textArr).toList().stream().filter(x -> !x.isEmpty()).toList();
+                    List<String> filtered = Arrays.stream(textArr).toList().stream().filter(x -> !x.isEmpty()).toList();
                     tempEle = filtered.get(0);
                     tempTime = filtered.get(1).replace("T", " ").replace("Z", "");
 
@@ -78,3 +76,4 @@ public class GPXParser {
         }
         return waypoints;
     }
+}
