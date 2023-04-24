3982080eb0ee6b63297e7e97f0316c69f3241004
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-04-24 13:57:35 +0300

    Workers and Server start with same method

--- MapaReduce/src/ServerMain.java
+++ MapaReduce/src/ServerMain.java
@@ -1,7 +1,14 @@
 import MasterServer.MasterServer;
+import WorkerClient.WorkerClient;
 
 public class ServerMain {
     public static void main(String args[]) {
+
         new MasterServer().openServer();
+
+        int numberOfWorkers = Integer.parseInt(args[0]);
+        for (int i = 0; i < numberOfWorkers; i++){
+                new WorkerClient().start(); //init
+        }
     }
 }
