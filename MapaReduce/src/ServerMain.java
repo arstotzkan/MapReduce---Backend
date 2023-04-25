b10ca05d91d4021b93fd59bf37b17a5760b356ca
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-04-25 14:10:32 +0300

    Server and Workers start on the same time

--- MapaReduce/src/ServerMain.java
+++ MapaReduce/src/ServerMain.java
@@ -1,9 +1,15 @@
 import MasterServer.MasterServer;
+import Worker.Worker;
 
 public class ServerMain {
     public static void main(String args[]) {
+        new MasterServer().start();
 
+        int numberOfWorkers = Integer.parseInt(args[0]);
+        for (int i = 0; i < numberOfWorkers; i++){
+            System.out.println();
+            new Worker(6000 + i).start(); //init
+        }
 
-        new MasterServer().openServer();
     }
 }
