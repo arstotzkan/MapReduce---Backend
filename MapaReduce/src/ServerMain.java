0d7544920f6092e0f488e7410511320a85021208
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-04-25 21:39:33 +0300

    numberOfWorkers is taken from input

--- MapaReduce/src/ServerMain.java
+++ MapaReduce/src/ServerMain.java
@@ -1,9 +1,17 @@
 import MasterServer.MasterServer;
 import Worker.Worker;
 
+import java.util.Scanner;
+
 public class ServerMain {
     public static void main(String args[]) {
-        int numberOfWorkers = Integer.parseInt(args[0]); //taking number of workers from args
+        int numberOfWorkers = -1;
+        // int numberOfWorkers = Integer.parseInt(args[0]); //taking number of workers from args
+        while (numberOfWorkers < 1 || numberOfWorkers > 1000) {
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Set number of workers (1-1000) :");
+            numberOfWorkers = myObj.nextInt(); //taking number of workers from args
+        }
         new MasterServer(numberOfWorkers).start();
         for (int i = 0; i < numberOfWorkers; i++){
             new Worker(6001 + i).start(); //init
