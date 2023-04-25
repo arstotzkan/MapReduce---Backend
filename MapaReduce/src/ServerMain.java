53db0b515e0db83ee0e5fb3a8e35f5e419518700
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-04-25 19:51:21 +0300

    Server knows the number of workers

--- MapaReduce/src/ServerMain.java
+++ MapaReduce/src/ServerMain.java
@@ -3,9 +3,8 @@ import Worker.Worker;
 
 public class ServerMain {
     public static void main(String args[]) {
-        new MasterServer().start();
-
         int numberOfWorkers = Integer.parseInt(args[0]);
+        new MasterServer(numberOfWorkers).start();
         for (int i = 0; i < numberOfWorkers; i++){
             new Worker(6000 + i).start(); //init
         }
