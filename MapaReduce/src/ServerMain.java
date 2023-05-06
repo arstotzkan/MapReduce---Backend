ead66f042280e6b6ba86f02edcb5e840ef30ebf5
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-05-06 11:40:02 +0300

    improved worker init

--- MapaReduce/src/ServerMain.java
+++ MapaReduce/src/ServerMain.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 
 public class ServerMain {
     public static void main(String args[]) {
-        int numberOfWorkers = -1;
-        // int numberOfWorkers = Integer.parseInt(args[0]); //taking number of workers from args
-        while (numberOfWorkers < 1 || numberOfWorkers > 1000) {
-            Scanner myObj = new Scanner(System.in);
-            System.out.println("Set number of workers (1-1000) :");
-            numberOfWorkers = myObj.nextInt(); //taking number of workers from args
+
+        int numberOfWorkers;
+        if (args.length > 0){
+            numberOfWorkers = Integer.parseInt(args[0]);
+        } else {
+            numberOfWorkers = -1;
+            while (numberOfWorkers < 1 || numberOfWorkers > 1000) {
+                Scanner myObj = new Scanner(System.in);
+                System.out.println("Set number of workers (1-1000) :");
+                numberOfWorkers = myObj.nextInt(); //taking number of workers from args
+            }
         }
+
         new MasterServer(numberOfWorkers).start();
         for (int i = 0; i < numberOfWorkers; i++){
             new Worker(60001 + i).start(); //init
