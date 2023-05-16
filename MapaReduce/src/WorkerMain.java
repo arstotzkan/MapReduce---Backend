98ac09c581b361d81f88d444012484bb8cd92ddb
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-05-16 16:11:54 +0300

    Made two WorkerMains

--- MapaReduce/src/WorkerMain.java
+++ MapaReduce/src/WorkerMain.java
@@ -5,21 +5,6 @@ import java.util.Scanner;
 
 public class WorkerMain {
     public static void main(String args[]) {
-
-        int numberOfWorkers;
-        if (args.length > 0){
-            numberOfWorkers = Integer.parseInt(args[0]);
-        } else {
-            numberOfWorkers = -1;
-            while (numberOfWorkers < 1 || numberOfWorkers > 1000) {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Set number of workers (1-1000) :");
-                numberOfWorkers = scanner.nextInt(); //taking number of workers from args
-            }
-        }
-
-        for (int i = 0; i < numberOfWorkers; i++){
-            new Worker(60012 + i).start(); //init
-        }
+        new Worker().start(); //init
     }
 }
